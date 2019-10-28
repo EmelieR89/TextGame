3ff43948154b9476bd95e0d37018b0a9f2fdd103
Emelie Rosenlöw <rosenlowemelie@gmail.com>
2019-10-28 19:08:47 +0100

    Changed the code in js and linked it to HTML

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
+let room = [];
 
-const storyBoard = document.querySelector("p")
-const inputAnswer = document.querySelector('input[name="Answer"]')
+room[0] = "Game over!!";
+room[1] = "office";
+room[2] = "library";
+room[3] = "kitchen";
+room[4] = "livingroom";
+room[5] = "master bedroom";
+room[6] = "lobby";
+room[7] = "music room";
+room[8] = "child room";
+console.log("kör")
+//Start location
+let currentRoom = 4;
 
-function toContinue() {
-    let userChoice = inputAnswer.value
+//array of commandos that tha game understands
+const commandosToMake = ["north", "east", "south", "west"];
 
-    color(userChoice)
+//input and output
+let output = document.getElementById("output");
+let input = document.getElementById("userAnswer");
+
+render();
+
+function playGame() {
+    let playersChoice = "";
+    let gameMessage = "";
+
+    playersChoice = input.value;
+    playersChoice = playersChoice.toLowerCase();
+
+    switch (playersChoice) {
+
+        case "north":
+            currentRoom -= 3;
+            break;
+
+        case "east":
+            currentRoom += 1;
+            break;
+
+        case "south":
+            currentRoom += 3;
+            break;
+
+        case "west":
+            currentRoom -= 1;
+            break;
+
+        default: gameMessage = "You can't make that choice"
+
+
+    }
+
+    render(gameMessage);
+
+
 }
 
-function color(colorChoice) {
-    let resultFromColorChoice = storyBoard.innerHTML
+function render(gameMessage = "") {
+    output.innerHTML = room[currentRoom];
 
-    if (colorChoice === "blue") {
-        resultFromColorChoice = "Du går in genom den blåa dörren. "
-            + "Du möter en gammal man."
+    output.innerHTML += "<br><em>" + gameMessage
+        + "</em>";
 
-    } else if (colorChoice === "red") {
-        resultFromColorChoice += "<br/>" + "<br/>" + "Du går in genom den röda dörren. " +
-            "Där ser du en låda."
-
-    } else if (colorChoice === "green") {
-        resultFromColorChoice = "Du dör."
-    }
-    else {
-        resultFromColorChoice += "<br/>" + "<br/>" + "The choice you made is invalid!"
-    }
-
-
-    printQuestion(resultFromColorChoice)
-}
-
-function printQuestion(newQuestion) {
-    storyBoard.innerHTML = newQuestion
 }
